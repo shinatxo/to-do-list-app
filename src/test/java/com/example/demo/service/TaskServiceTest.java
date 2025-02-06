d482f383da5b8d7c533ff23751d5447f51b57a51
Shina <shinaoguntoye@hotmail.co.uk>
2025-02-06 08:26:58 +0000

    test: add unit tests for TaskService and exclude JUnit Vintage engine in pom.xml

--- src/test/java/com/example/demo/service/TaskServiceTest.java
+++ src/test/java/com/example/demo/service/TaskServiceTest.java
@@ -16,7 +16,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-class TaskServiceTest {
+public class TaskServiceTest {
 
     @Mock
     private TaskRepository taskRepository;
